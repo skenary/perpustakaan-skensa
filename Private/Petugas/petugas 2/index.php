f55e3bdf25065bbc336cc4fcf009000b69ba104c
advenrihimone <advenrihimone@gmail.com>
2020-04-08 08:17:19 +0800

    petugas 2
    tampilan petugas 2 siap

--- Private/Petugas/petugas 2/index.php
+++ Private/Petugas/petugas 2/index.php
@@ -1,33 +1,48 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Skenary</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Petugas 2</title>
+	<link rel="stylesheet" href="../../../asset/css/petugas2(1).css">
 </head>
 <body>
-	<?php 
-	
-		if(isset($_GET['pesan'])){
-			if($_GET['pesan']=="error"){
-				$pesan = "Anda tidak memiliki hak akses !";
-				echo "<script type='text/javascript'>alert('$pesan');</script>";	
-			}
-		}
-
-		session_start();
-	 
-		if($_SESSION['level']==""){
-			header("location:index.php?pesan=error");
-		} else if($_SESSION['level']=="petugas1"){
-			header("location:../petugas 1/index.php?pesan=error");
-		} else if($_SESSION['level']=="petugas3"){
-			header("location:../petugas 1/index.php?pesan=error");
-		}
- 
-	?>
-	
-	<h1>Halaman Pegawai 2</h1>
- 
-	<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b><?php echo $_SESSION['level']; ?></b>.</p>
-	<a href="../../logout.php">LOGOUT</a>
+	<div class="bg"></div>
+	<div class="opening">
+		<h1>Hai Petugas 2!</h1>
+		<h5>Welcome back to Skenary</h5>
+	</div>
+	<!-- header -->
+	<div class="header">
+		<table width="100%" class="tbheader">
+			<tr>
+				<td width="20%" >
+				<h1 class="logo">Skenary</h1>
+				</td>
+				<td width="50%"></td>
+				<td width="12.5%">
+				<a href="#" class="active">Book Borrowed</a>
+				</td>
+				<td width="12.5%">
+				<a href="#" class="nav">Book Borrow</a>
+				</td>
+				<td>
+				<img src="../../../asset/img/menunav.jpg" class="menunav" alt="">
+				</td>
+			</tr>
+		</table>
+  	</div>
+  <!-- header -->
+	<div class="box">
+		<h3>Student Information</h3>
+		<p>12 January 2020</p>
+		<h5>Fill The Box</h5>
+		<form action="">
+			<input type="text" placeholder="NIS" name="" required><br>
+			<input type="text" placeholder="Book Code" name="" required><br>
+			<input type="text" placeholder="Book Name" name="" required><br>
+			<button type="submit">Enter</button>
+		</form>
+	</div>
 </body>
 </html>
