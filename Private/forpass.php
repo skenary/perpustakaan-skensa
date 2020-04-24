cf5fdff7bfda1b5e9fad6899cee0f5b0d060d314
advenrihimone <advenrihimone@gmail.com>
2020-04-24 08:21:31 +0800

    membuat form forgot password

---
+++ Private/forpass.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Forgot Password</title>
+  <link rel="stylesheet" href="../asset/css/forpass.css">
+</head>
+<body>
+
+	<?php 
+
+	?>
+ 
+	<center>
+    <div class="img">
+    	<form action="" method="">
+	    <div class="box1">
+	      <center>
+	        <h1>Forgot Password</h1>
+            <p>You can reset your password here.</p>
+	      </center>
+	      <div class="box2">
+	        <div class="form">
+	          <input type="text" id="email" name="email" required="" autocomplete="off">
+	          <label for="email" class="label-email">
+	              <span class="content-email">Email</span>
+	          </label>
+	        </div>
+	        <input type="submit" onclick="" name="submit" class="login" value="Submit">
+	    </div>
+	    </form>
+    </div>
+  </center>
+</body>
+</html>
